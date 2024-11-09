f99e43dca9cd2c94ac4c6425c95183a2cf66bfc2
NITHINKR06 <146012853+NITHINKR06@users.noreply.github.com>
2024-11-09 17:15:43 +0530

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,25 @@
+// const mongoose = require("mongoose");
+// const colors = require("colors");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(`mongodb://localhost:27017/`);
+//     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+//   } catch (error) {
+//     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
+//   }
+// };
+
+// module.exports = connectDB;
+
+const mongoose = require("mongoose");
+const ConnectURI = "mongodb://localhost:27017/Health";
+const ConnectToMongo = async () => {
+  try {
+    await mongoose.connect(ConnectURI);
+    console.log("Connect To Mongo Successful");
+  } catch (err) {
+    console.log("Connect To Mongo Unsuccessful", err);
+  }
+};
+module.exports = ConnectToMongo;
